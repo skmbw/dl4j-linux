293f72e77319b2ed7136a7482a5f0d4078f89f31
skmbw <tongku2008@126.com>
2017-02-27 11:28:25 +0800

    将opencv河tesseract结合起来

--- src/main/java/net/sourceforge/tess4j/Word.java
+++ src/main/java/net/sourceforge/tess4j/Word.java
@@ -18,7 +18,7 @@ package net.sourceforge.tess4j;
 import java.awt.Rectangle;
 
 /**
- * Encapsulates Tesseract OCR results.
+ * Encapsulates Tesseract OCR results.ceshi yxiia
  */
 public class Word {
 
